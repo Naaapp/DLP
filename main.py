3f3ec8dc2fd44f24c922f4d82b8894ebd141b0c9
Théo <metroid.stassen@gmail.com>
2020-12-11 21:57:49 +0100

    main

---
+++ main.py
@@ -0,0 +1,7 @@
+# Import the tensorflow library
+import tensorflow
+# Import the keras library
+import keras
+
+print('tensorflow: %s' % tensorflow.__version__)
+print('keras: %s' % keras.__version__)
